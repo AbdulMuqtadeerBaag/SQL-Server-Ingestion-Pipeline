08d60b4204492af3a2cde23d7e30c18adc251cbb
AbdulMuqtadeerBaag <muqtadeerbaag@gmail.com>
2025-12-07 16:00:54 +0530

    Update 2.1_Create_Raw_Tables.sql

--- Scripts/02_Raw_Layer/2.1_Create_Raw_Tables.sql
+++ Scripts/02_Raw_Layer/2.1_Create_Raw_Tables.sql
@@ -1,1 +1,20 @@
+/*
+==========================================================
+              DDL Scripts: Create Raw Tables
+==========================================================
+Project: SQL Server Ingestion Pipeline
+Scripts: 2.1_Create_Raw_Tables.sql
+Author: Abdul Muqtadeer Baag
+Created On: 07/12/2025
 
+Purpose:
+    Create raw tables to store incoming CSV data (Exact Copy).
+
+Warning!
+    This table holds raw unmodified source data.
+    Do not apply any transformations here.
+*/
+
+-- Use the Database:
+  USE IngestionDB;
+  GO
