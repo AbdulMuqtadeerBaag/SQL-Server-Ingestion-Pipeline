8e4622c5d7350a308fadae3e8becff587ad648cd
AbdulMuqtadeerBaag <muqtadeerbaag@gmail.com>
2025-12-11 14:34:24 +0530

    Add quality checks for final layer data integrity
    This script performs quality checks on the final layer of the SQL Server Ingestion Pipeline, ensuring data integrity and consistency across dimension and fact views.

--- Scripts/04_Final_Layer/4.2_Quality_Checks_Final.sql
+++ Scripts/04_Final_Layer/4.2_Quality_Checks_Final.sql
@@ -1,1 +1,69 @@
+/*
+==================================================================================
+                  DQL Script: Final Layer Quality Checks
+==================================================================================
 
+Project     : SQL Server Ingestion Pipeline
+Script      : 4.2_Quality_Checks_Final.sql
+Author      : Abdul Muqtadeer Baag
+
+Purpose:
+	- Validate the integrity and consistency of the 'final' layer.
+	- This scripts checks for:
+		  - Unqiueness of surrogate keys in dimension views.
+		  - Referential integrity between fact and dimension views.
+		  - Missing or invaild relationships in the star-schema.
+
+Usage Notes:
+	- Run these checks after creating 'final' dimensions and fact views.
+	- Investigate and fix any issues found by these queries.
+
+===================================================================================
+*/
+
+-- Change the data context:
+	USE IngestionDB;
+	GO
+
+-- ================================================================================
+--					1. Checking 'final.dim_customers'
+-- ================================================================================
+
+-- Check for uniqueness of customer key in final.dim_customers:
+-- Expectation: No results:
+SELECT
+	Customer_Key,
+	COUNT(*) AS Duplicate_Count
+FROM final.dim_customers
+GROUP BY Customer_Key
+HAVING COUNT(*)>1;
+GO
+
+-- ================================================================================
+--					2. Checking 'final.dim_products'
+-- ================================================================================
+
+-- Check for uniqueness of product key in final.dim_products:
+-- Expectation: No results:
+SELECT
+	Product_Key,
+	COUNT(*) AS Duplicate_Count
+FROM final.dim_products
+GROUP BY Product_Key
+HAVING COUNT(*)>1;
+GO
+
+-- ================================================================================
+--					3. Checking 'final.fact_sales'
+-- ================================================================================
+
+-- Check the date model connectivity between dimensionS and fact:
+SELECT
+	*
+FROM final.fact_sales S
+LEFT JOIN final.dim_customers C
+	ON C.Customer_Key = S.Customer_Key
+LEFT JOIN final.dim_products P
+	ON P.Product_Number = S.Product_Key
+WHERE P.Product_Number IS NULL OR C.Customer_Key IS NULL;
+GO
